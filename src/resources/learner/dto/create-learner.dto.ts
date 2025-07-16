6da9e6c215a55aa971a46444791355a055b64c3d
Bismark <agyarebismark6@gmail.com>
2025-07-16 23:10:57 +0000

    Implement learner dto

--- src/resources/learner/dto/create-learner.dto.ts
+++ src/resources/learner/dto/create-learner.dto.ts
@@ -1,1 +1,29 @@
-export class CreateLearnerDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateLearnerDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  first_name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  last_name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty()
+  @IsStrongPassword()
+  @IsNotEmpty()
+  password: string;
+}
